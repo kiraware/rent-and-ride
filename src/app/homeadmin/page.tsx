7e3325834239b02585af6ed538d2ffe0b1ad3ae7
JoeSinuraya <joefebriansinuraya@gmail.com>
2024-05-20 20:49:17 +0700

    halaman homeadmin

---
+++ src/app/homeadmin/page.tsx
@@ -0,0 +1,72 @@
+export default function Pesanan() {
+    return (
+      <main>
+      {/* <div class="container">
+    <div class="shape1">
+        <img src="https://placehold.co/150x100" alt="">
+        <h4>INNOVA REBORN</h4><br>
+        <div class="shape5"><br>
+            <h4>Rp. 550rb/Hari</h4>
+        </div><br><br>
+        <h5>Toyota Silver MPV</h5><br>
+        <button class="button1">
+            Hapus
+        </button>
+        <button class="button2">
+            Sunting
+        </button>
+    </div>
+
+    <div class="shape2">
+        <img src="https://placehold.co/150x100" alt="">
+        <h4>HIACE COMMUTER</h4><br>
+        <div class="shape6"><br>
+            <h4>Rp. 800rb/Hari</h4>
+        </div><br><br>
+        <h5>Toyota Silver Minivans</h5><br>
+        <button class="button3">
+            Hapus
+        </button>
+        <button class="button4">
+            Sunting
+        </button>
+    </div>
+
+    <div class="shape3">
+        <img src="https://placehold.co/150x100" alt="">
+        <h4>CALYA</h4><br>
+        <div class="shape7"><br>
+            <h4>Rp. 300rb/Hari</h4>
+        </div><br><br>
+        <h5>Toyota Silver SUV</h5><br>
+        <button class="button5">
+            Hapus
+        </button>
+        <button class="button6">
+            Sunting
+        </button>
+    </div>
+
+    <div class="shape4">
+        <img src="https://placehold.co/150x100" alt="">
+        <h4>PAJERO</h4><br>
+        <div class="shape8"><br>
+            <h4>Rp. 900rb/Hari</h4>
+        </div><br><br>
+        <h5>Mitsubishi Silver SUV</h5><br>
+        <button class="button7">
+            Hapus
+        </button>
+        <button class="button8">
+            Sunting
+        </button>
+    </div>
+</div><br><br><br><br>
+
+<div class="container">
+    <button class="button">Tambah</button>
+</div><br><br><br><br><br><br><br><br></br> */}
+      </main>
+    )
+  }
+  
