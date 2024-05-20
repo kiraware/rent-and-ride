99b19c4f05ee579a5e8095d8fdd1ec5900ac6f2f
JoeSinuraya <joefebriansinuraya@gmail.com>
2024-05-20 20:33:58 +0700

    halaman pesanan

--- src/app/pesanan/page.tsx
+++ src/app/pesanan/page.tsx
@@ -1,7 +1,41 @@
 export default function Pesanan() {
   return (
     <main>
-      
+    {/* <h1>Pesanan Kendaraan</h1><br><br><br>
+
+<div class="container">
+    <div class="shape1">
+        <img src="https://placehold.co/150x100" alt=""><br>
+        <h4>INNOVA REBORN</h4><br>
+        <h5>Toyota Silver MPV</h5><br><br>
+        <h4>Belum Bayar</h4>
+        <h4>20 Januari 2024 - 23 Januari 2024</h4>
+        <h4>Rp. 2.200.000</h4>
+        <h4>Joe Febrian Sinuraya</h4><br><br>
+        <button class="button1">
+            Batalkan Pesanan
+        </button><br>
+        <button class="button2">
+            Ubah Status
+        </button>
+    </div>
+
+    <div class="shape2">
+        <img src="https://placehold.co/150x100" alt=""><br>
+        <h4>HIACE COMMUTER</h4><br>
+        <h5>Toyota Silver Minivans</h5><br><br>
+        <h4>Sudah Bayar</h4>
+        <h4>02 Januari 2024</h4>
+        <h4>Rp. 550.000</h4>
+        <h4>Muhammad Fajar Alfad</h4><br><br><br>
+        <button class="button3">
+            Batalkan
+        </button>
+        <button class="button4">
+            Ubah Status
+        </button>
+    </div>
+</div><br><br><br> */}
     </main>
   )
 }
