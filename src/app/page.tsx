d7b4d356e511c359b84b8d4ca4a1259a63395116
kiraware <117554978+kiraware@users.noreply.github.com>
2024-05-21 10:05:56 +0800

    add homepage and add image placehold config (#4)
    We create user homepage, and for the vehicle image we use placeholder image, therefore we need to configure the next config too.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,43 @@
+import CarOrderFigure from '@/components/CarOrderFigure'
+import Nav from '@/components/Nav'
+import { getVehicles } from '@/lib/vehicles'
+import * as jose from 'jose'
+import { cookies } from 'next/headers'
+
 export default async function Home() {
-  return <main></main>
+  const jwt = cookies().get('Authorization')
+  let isAdmin = false
+
+  if (jwt !== undefined) {
+    const claims = jose.decodeJwt(jwt.value)
+    isAdmin = !!claims.isAdmin
+  }
+
+  const vehicles = await getVehicles()
+
+  return (
+    <>
+      <header className="py-8">
+        <Nav />
+      </header>
+
+      {isAdmin ? (
+        <></>
+      ) : (
+        <>
+          <h1 className="text-center text-4xl font-bold">Rent & Ride</h1>
+          <p className="mx-auto w-1/2 py-4 text-center text-xl font-semibold">
+            Layanan rental kendaraan untuk perusahaan maupun personal yang
+            mengutamakan kenyamanan serta keamanan pelanggan
+          </p>
+
+          <section className="flex flex-row flex-wrap justify-evenly">
+            {vehicles.map((vehicle) => (
+              <CarOrderFigure key={vehicle.id} vehicle={vehicle} />
+            ))}
+          </section>
+        </>
+      )}
+    </>
+  )
 }
