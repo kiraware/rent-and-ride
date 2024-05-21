11011090ab105ec25f5eb89a5ca5348eb9d9e665
fajaralfad <fajaralfad85@gmail.com>
2024-05-21 12:21:21 +0700

    riwayat

--- src/app/riwayat/page.tsx
+++ src/app/riwayat/page.tsx
@@ -1,7 +1,47 @@
 export default function Riwayat() {
   return (
     <main>
-      
+{/* 
+      <h1>
+        Riwayat Sewa Kendaraan
+      </h1>
+
+        <div class="wrapper"> 
+          <div class="card">
+            <img src="https://placehold.co/400x400">
+              <div class="info">
+                <h1>"Nama kendaraan(bold)"</h1>
+                <h1>"Harga"</h1>
+                <h1>"Jenis Mobil"</h1>
+            </div>
+          </div>
+        </div>
+
+        <div class="wrapper">
+          <div class="card">
+            <img src="https://placehold.co/400x400">
+              <div class="info">
+                <h1>"Nama kendaraan(bold)"</h1>
+                <h1>"Warna dan jenis"</h1>
+                <h1>"Tanggal Pemakaian"</h1>
+                <h1>"harga"</h1>
+            </div>
+          </div>
+        </div>
+
+        <div class="wrapper">
+          <div class="card">
+            <img src="https://placehold.co/400x400">
+              <div class="info">
+                <h1>"Nama kendaraan(bold)"</h1>
+                <h1>"Warna dan jenis"</h1>
+                <h1>"Tanggal Pemakaian"</h1>
+                <h1>"harga"</h1>
+            </div>
+          </div>
+        </div> */}
+
     </main>
-  )
-}
+
+  ]
+)
