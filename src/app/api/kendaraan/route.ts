217465a9db0f740d5914789b065a1b1a11a105d7
kiraware <117554978+kiraware@users.noreply.github.com>
2024-06-09 23:31:13 +0800

    Edit Vehicle Page (#13)
    - create editVehicleAction
    - create PUT route
    - create edit vehicle page
    - create CarShowFigure2 component
    - create EditVehicleForm
    - create EditVehicleSchema

--- src/app/api/kendaraan/route.ts
+++ src/app/api/kendaraan/route.ts
@@ -1,5 +1,5 @@
 import prisma from '@/lib/prisma'
-import { CreateVehicleSchema } from '@/schemas'
+import { CreateVehicleSchema, EditVehicleSchema } from '@/schemas'
 import { ColorEnum, MerkEnum, VehicleTypeEnum } from '@prisma/client'
 
 export async function POST(request: Request) {
@@ -23,6 +23,55 @@ export async function POST(request: Request) {
 
   // Create the vehicle
   const vehicle = await prisma.vehicle.create({
+    data: {
+      name,
+      image,
+      merk: merk as MerkEnum,
+      color: color as ColorEnum,
+      type: type as VehicleTypeEnum,
+      price: Number(price),
+    },
+  })
+
+  if (!vehicle) {
+    return Response.json(
+      {
+        error: 'Invalid name or image or merk or color or type or price',
+      },
+      { status: 400 },
+    )
+  }
+
+  // Respond something
+  return Response.json({})
+}
+
+export async function PUT(request: Request) {
+  // Read data off req body
+  const body = await request.json()
+
+  console.log(body)
+
+  // Validate data
+  const validatedFields = EditVehicleSchema.safeParse(body)
+
+  if (!validatedFields.success) {
+    return Response.json(
+      {
+        error: 'Invalid id or name or image, merk, color, type, price',
+      },
+      { status: 400 },
+    )
+  }
+
+  // Read data off validated fields
+  const { id, name, image, merk, color, type, price } = validatedFields.data
+
+  // Update the vehicle
+  const vehicle = await prisma.vehicle.update({
+    where: {
+      id,
+    },
     data: {
       name,
       image,
