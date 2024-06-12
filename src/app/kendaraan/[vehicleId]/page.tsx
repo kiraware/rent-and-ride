4396b391a55318c8ee3246fced35ba7035dc0c33
fajaralfad <fajaralfad85@gmail.com>
2024-06-13 01:01:08 +0700

    update

--- src/app/kendaraan/[vehicleId]/page.tsx
+++ src/app/kendaraan/[vehicleId]/page.tsx
@@ -37,9 +37,9 @@ export default async function EditKendaraan({ params: { vehicleId } }: Props) {
   if (!vehicle) notFound()
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-4">
+    <main className="flex min-h-screen flex-col items-center justify-start gap-4">
       <h1 className="w-fit pb-16 text-4xl font-bold after:mx-auto after:flex after:h-1 after:w-4/6 after:rounded-full after:bg-blue-600">
-        Sunting Kendaraan
+        Sunting kendaraan
       </h1>
 
       <section className="flex flex-row gap-4">
--- src/app/kendaraan/page.tsx
+++ src/app/kendaraan/page.tsx
@@ -2,7 +2,7 @@ import CreateVehicleForm from '@/components/CreateVehicleForm'
 
 export default function Kendaraan() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-4">
+    <main className="flex min-h-screen flex-col items-center justify-start gap-4">
       <h1 className="w-fit pb-16 text-4xl font-bold after:mx-auto after:flex after:h-1 after:w-4/6 after:rounded-full after:bg-blue-600">
         Tambah kendaraan
       </h1>
