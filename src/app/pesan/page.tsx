f8102e12ff95f6fbdac8a4e6c5dae53693aa7f27
fajaralfad <fajaralfad85@gmail.com>
2024-06-13 00:34:50 +0700

    update

--- src/app/pesan/page.tsx
+++ src/app/pesan/page.tsx
@@ -16,9 +16,9 @@ export default async function Pesan() {
   const pendingOrPaidOrders = await getPendingOrPaidOrdersByUserId(userId)
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-4">
+    <main className="flex min-h-screen flex-col items-center justify-start gap-4">
       <h1 className="w-fit pb-16 text-4xl font-bold after:mx-auto after:flex after:h-1 after:w-4/6 after:rounded-full after:bg-blue-600">
-        Daftar Pesanan Kendaraan
+        Daftar pesan sewa kendaraan
       </h1>
 
       <section className="flex flex-row flex-wrap justify-evenly gap-4">
